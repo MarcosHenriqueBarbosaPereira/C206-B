3b04769cf4e2b0ed293dfcb74a1e6d62f520a05b
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2023-04-05 22:51:11 -0300

    Aula 6

---
+++ Aula 31-03/src/br/inatel/cdg/inimigo/Asteroide.java
@@ -0,0 +1,16 @@
+package br.inatel.cdg.inimigo;
+
+public class Asteroide {
+
+    String nome;
+    String tipoAsteroide;
+
+    public Asteroide(String nome, String tipoAsteroide){
+        this.nome = nome;
+        this.tipoAsteroide = tipoAsteroide;
+    }
+
+    void destruir(){
+        
+    }
+}
---
+++ Aula 31-03/src/br/inatel/cdg/jogador/Nave.java
@@ -0,0 +1,19 @@
+package br.inatel.cdg.jogador;
+
+public class Nave {
+
+    String nome;
+    double vida;
+    String tiroTipo;
+
+    public Nave(String nome, double vida, String tipoTiro){
+        this.nome = nome;
+        this.vida = vida;
+        this.tiroTipo = tipoTiro;
+    }
+
+    void atirar(){
+
+    }
+
+}
